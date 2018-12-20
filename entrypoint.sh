b9bbcf81a6fcf06d074de01b8bf163631df681e1
Maxence Maireaux <maxence@maireaux.fr>
2018-12-20 17:54:18 +0100

    Add SSH Private key

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 set -e
 cd "${ANSIBLE_ACTION_WORKING_DIR:-.}"
 
+if [  ! -z "$SSH_PRIVATE_KEY" ]; then
+  echo "${SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
+fi
+
 if [ "$USE_GALAXY" = "true" ]; then
   ansible-galaxy install -r ${ANSIBLE_ACTION_GALAXY_FILE}
 fi
